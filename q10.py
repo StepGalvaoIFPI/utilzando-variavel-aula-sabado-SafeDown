44e3003bd602947c9459428ba9d97b7d1c4489e7
SafeDown <132012360+SafeDown@users.noreply.github.com>
2025-04-15 20:37:32 -0300

    Update q10.py

--- q10.py
+++ q10.py
@@ -1,1 +1,12 @@
+salarioBruto = 5000
+impostoInss = 0.15*salarioBruto
+impostoIr = 0.10*salarioBruto
+salarioLiquido = salarioBruto-impostoInss+impostoIr
 
+print("Próximo exercício Questão 10")
+print("Salario Liquido:")
+print(salarioLiquido)
+print("Desconto do INSS")
+print(impostoInss)
+print("Desconto do IR")
+print(impostoIr)
