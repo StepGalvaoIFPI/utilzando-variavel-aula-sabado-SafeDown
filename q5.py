3bb0d15b2b4bdf2c100241e2cd98d905078a16b0
SafeDown <132012360+SafeDown@users.noreply.github.com>
2025-04-15 20:39:00 -0300

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,1 +1,15 @@
-
+print("Próximo exercício Questão 5")
+ 
+num1 =10
+num2 =5
+num3 =8
+media = (num1+num2+num3)/3
+ 
+print("Soma dos valores: num1=10 + num2=5 + num3=8")
+print("Resultado:")
+print(media)
+ 
+print("Agora exiba a media...")
+print("Resultado:")
+ 
+print(media)
